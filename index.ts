50068ef671b1d2dc5522b42610cc8b349419f93b
Ryan Rudd <ryanrudd.dev@gmail.com>
2025-12-15 20:27:25 -0500

    basic bot mention test

--- index.ts
+++ index.ts
@@ -1,9 +1,15 @@
 import { Client, Events, GatewayIntentBits } from "discord.js";
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
 client.once(Events.ClientReady, c => {
     console.log(`Logged in as ${c.user.tag}`);
 });
+
+client.on(Events.MessageCreate, message => {
+    if (message.mentions.has(client.user!)) {
+        message.reply("Hello! You mentioned me!");
+    }
+})
 
 client.login(process.env.DISCORD_TOKEN)
